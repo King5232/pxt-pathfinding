1b8960e063ebc24fee40ab2e07403c5b927a735c
King5232 <160287903+King5232@users.noreply.github.com>
2024-11-14 18:27:30 -0600

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -103,8 +103,9 @@ namespace pathfinding {
     }
 
     //% block="set enemies follow $player2 with speed $speed"
-    //% player2.defl=someName
+    //% player2.defl=myPlayer
     //% player2.shadow=variables_get
+    //% speed.defl=100
     export function start_enemies_following_player_with_speed(player2: Sprite, speed: number) {
         game.onUpdate(function () {
             for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
